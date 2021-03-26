4452700572cda9f229289ffc44a7000d1e3bcfac
navono <navono007@gmail.com>
2021-03-26 18:23:36 +0800

    redux

--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import { TestComponentProps } from './TestComponent.types';
 import './TestComponent.scss';
 import Counter from '../Counter';
-import { store } from '../store';
+import counterReducer from '../Counter/counterSlice';
 
-const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
-  <Provider store={store}>
-    <div
-      data-testid="test-component"
-      className={`test-component test-component-${theme}`}
-    >
-      <h1 className="heading">I'm the test component</h1>
+const TestComponent: React.FC<TestComponentProps> = ({ theme }) => {
+  const store = configureStore({
+    reducer: {
+      counter: counterReducer,
+    },
+  });
+  return (
+    <Provider store={store}>
       <Counter />
-      <h2>Made with love by Harvey</h2>
-    </div>
-  </Provider>
-);
+      <div
+        data-testid="test-component"
+        className={`test-component test-component-${theme}`}
+      >
+        <h1 className="heading">I'm the test component</h1>
+        <h2>Made with love by Harvey</h2>
+      </div>
+    </Provider>
+  );
+};
 
 export default TestComponent;
